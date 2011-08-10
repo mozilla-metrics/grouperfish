e97d2b4a043663d13a54b8f309b806629c0fc99c
Michael Kurze <michael@thefoundation.de>
2011-08-10 09:40:46 -0700

    Simplified startup by invoking Bagheera's startup code.

--- service/src/main/java/com/mozilla/grouperfish/service/Documents.java
+++ service/src/main/java/com/mozilla/grouperfish/service/Documents.java
@@ -5,8 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 @Path("/documents")
-public class DocumentsResource {
-    
+public class Documents {
 
     @GET
     @Produces("text/plain")
--- service/src/main/java/com/mozilla/grouperfish/service/Grouperfish.java
+++ service/src/main/java/com/mozilla/grouperfish/service/Grouperfish.java
@@ -1,17 +1,9 @@
 package com.mozilla.grouperfish.service;
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.config.MapConfig;
-import com.hazelcast.core.Hazelcast;
-import com.sun.jersey.spi.container.servlet.ServletContainer;
+import com.mozilla.bagheera.rest.Bagheera;
 
 
 /** Entry class to  set up the Grouperfish service. */
@@ -20,50 +12,17 @@ public class Grouperfish {
     static Logger log = LoggerFactory.getLogger(Grouperfish.class);
 
     /**
+     * Arguments:
+     * - port number for the Grouperfish/Bagheera REST service
+     * - HazelCast config file
      * 
-     * @param args
+     * @param arguments
      * @throws Exception 
      */
-	public static void main(String[] args) throws Exception {
-	    
-	    // Initialize HazelCast
-	    
-	    Hazelcast.getDefaultInstance();
-        Config config = Hazelcast.getConfig();
-        log.debug("Checking map: {}", "documents_test");
-        Hazelcast.getMap("documents_test");
-        for (Map.Entry<String, MapConfig> entry : config.getMapConfigs().entrySet()) {
-            String mapName = entry.getKey() + "test";
-            log.debug("Checking map: {}", mapName);
-            Hazelcast.getMap(mapName);
-        }
+	public static void main(String[] arguments) throws Exception {
 
+	    Bagheera.main(arguments);
         
-        // Initialize Jersey
-	    
-        ServletHolder jerseyHolder = new ServletHolder(ServletContainer.class);
-        /*
-        jerseyHolder.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", 
-                                      "com.sun.jersey.api.core.PackagesResourceConfig");
-                                      */
-        jerseyHolder.setInitParameter("com.sun.jersey.config.property.packages", 
-                                      "com.mozilla.grouperfish.service");
-        jerseyHolder.setInitOrder(1);
-        
-
-        // And Jetty
-        
-        int port = Integer.parseInt(System.getProperty("server.port", "8080"));
-        Server server = new Server(port);
-        ServletContextHandler root = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
-        root.addServlet(jerseyHolder, "/*");
-        
-        server.setSendServerVersion(false);
-        server.setSendDateHeader(false);
-        server.setStopAtShutdown(true);
-
-        server.start();
-        server.join();
 	}
 
 }
