2e3746cbf4d51d0ae7b11c16429bc5996715b4b3
Michael Kurze <michael@thefoundation.de>
2011-09-20 00:04:48 +0200

    Stream query results onto (d)fs using an ElasticSearch scroller. Allows to handle arbitrarily large input sets.

--- service/src/main/java/com/mozilla/grouperfish/services/elasticsearch/ElasticSearchIndex.java
+++ service/src/main/java/com/mozilla/grouperfish/services/elasticsearch/ElasticSearchIndex.java
@@ -1,13 +1,15 @@
 package com.mozilla.grouperfish.services.elasticsearch;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.action.search.SearchRequestBuilder;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,22 +35,13 @@ public class ElasticSearchIndex implements com.mozilla.grouperfish.services.api.
 
     @Override
     public Iterable<Document> find(final Query query) {
+        return new Iterable<Document>() {
+            @Override
+            public Iterator<Document> iterator() {
+                return new Pager(query);
+            }
 
-        final SearchRequestBuilder requestBuilder =
-            client.prepareSearch(indexName).setTypes(type).setExtraSource(query.source());
-        final SearchRequest request = requestBuilder.request();
-        final SearchResponse response = client.search(request).actionGet();
-
-        // :TODO: Optimize:
-        // Smarter implementation of the iterable would allow to spit results from scroller...
-        final List<Document> results = new ArrayList<Document>();
-        for (final SearchHit hit : response.hits()) {
-            results.add(new Document(hit.getId(), hit.getSource()));
-        }
-
-        log.debug(String.format("Returning %s/%s documents from find (index: %s, type: %s, query: %s)...",
-                                results.size(), response.hits().totalHits(), indexName, type, query.source()));
-        return results;
+        };
     }
 
     @Override
@@ -57,6 +50,70 @@ public class ElasticSearchIndex implements com.mozilla.grouperfish.services.api.
         // :TODO: Template queries
         Assert.unreachable("Not implemented.");
         return null;
+    }
+
+
+    private static final int BATCH_SIZE = 2048;
+    private static final TimeValue TIMEOUT = TimeValue.timeValueSeconds(30);
+
+    class Pager implements Iterator<Document> {
+
+        final Query query;
+        final String scrollId;
+        final long totalHits;
+
+        long position = 0;
+        int positionInBatch = 0;
+        SearchHits batch = null;
+
+        public Pager(final Query query) {
+            this.query = query;
+
+            final SearchResponse response = client.prepareSearch(indexName)
+                    .setTypes(type)
+                    .setSize(BATCH_SIZE)
+                    .setScroll(TIMEOUT)
+                    .setExtraSource(query.source()).execute().actionGet();
+
+            batch = response.hits();
+
+            scrollId = response.getScrollId();
+            totalHits = batch.getTotalHits();
+
+            log.debug(String.format("First batch: got %s/%s documents (index: %s, type: %s, query: %s)...",
+                                    batch.hits().length, totalHits, indexName, type, query.name()));
+            log.trace("Query: {}", query.source());
+        }
+
+        @Override
+        public boolean hasNext() {
+            return position < totalHits;
+        }
+
+        @Override
+        public Document next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("Tried to get more docs than available.");
+            }
+            if (positionInBatch == BATCH_SIZE) {
+                final SearchResponse response =
+                        client.prepareSearchScroll(scrollId).setScroll(TIMEOUT).execute().actionGet();
+                batch = response.hits();
+                positionInBatch = 0;
+                log.debug(String.format("Next batch: got %s/%s documents (index: %s, type: %s, query: %s)...",
+                                        position + batch.hits().length, totalHits, indexName, type, query.name()));
+
+            }
+            final SearchHit next = batch.getAt(positionInBatch);
+            ++positionInBatch;
+            ++position;
+            return new Document(next.getId(), next.getSource());
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 
 }
